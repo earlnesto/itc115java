1097b10b48381a71c63d8940d17c8e8012d972a0
rebyers <42394121+rebyers@users.noreply.github.com>
2018-10-22 15:34:56 -0700

    Update assignment3.java

--- assignment3.java
+++ assignment3.java
@@ -1,1 +1,28 @@
 
+public class Assignment3 {
+
+      public static int printPowersOfN (int base, int exponent) {
+
+          int result = 1;
+
+          for (int i = 1; i <= exponent; i++) {
+        	  System.out.print(result);
+        	  System.out.print(" ");
+        	  
+              result *=base;
+
+          }
+
+          return result;
+
+     }
+
+     public static void main(String[] args) {
+
+          System.out.println(printPowersOfN(4, 3));
+          System.out.println(printPowersOfN(5, 6));
+          System.out.println(printPowersOfN(-2, 8));
+
+     }
+
+}
